8f7f31614f5d8c5fdf9140472dd9112ed8d02f48
SIDDH Jain <siddhjain@SIDDHs-MacBook-Air.local>
2025-04-23 19:01:26 +0530

    removed react-native-dotenv setup, does not work

--- app/components/LandmarkDropdownFilters.tsx
+++ app/components/LandmarkDropdownFilters.tsx
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet, TextInput, TouchableOpacity, FlatList, Activity
 import Slider from '@react-native-community/slider';
 import { Landmark } from '../(tabs)/properties';
 import { Ionicons } from '@expo/vector-icons';
-import { PLACES_API_KEY } from '@env';
+// import { PLACES_API_KEY } from '@env';
 
 // Define types for API responses
 interface PlacePrediction {
@@ -27,7 +27,8 @@ interface LandmarkDropdownFiltersProps {
 }
 
 // Note: In production, use environment variables or a secure config approach
-const API_KEY = PLACES_API_KEY;
+// const API_KEY = PLACES_API_KEY;
+const API_KEY = "AIzaSyBsygl4y777lWd7M7mMQMwvnTyYFjPwoaM";
 
 const LandmarkDropdownFilters = ({ selectedLandmark, setSelectedLandmark }: LandmarkDropdownFiltersProps) => {
   const [searchQuery, setSearchQuery] = useState('');
--- app/config/firebase.ts
+++ app/config/firebase.ts
@@ -1,29 +1,29 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-import { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, FIREBASE_DATABASE_URL, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_APP_ID, FIREBASE_MEASUREMENT_ID } from '@env';
+// import { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, FIREBASE_DATABASE_URL, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_APP_ID, FIREBASE_MEASUREMENT_ID } from '@env';
 
-
-const firebaseConfig = {
-  apiKey: FIREBASE_API_KEY,
-  authDomain: FIREBASE_AUTH_DOMAIN,
-  databaseURL: FIREBASE_DATABASE_URL,
-  projectId: FIREBASE_PROJECT_ID,
-  storageBucket: FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
-  appId: FIREBASE_APP_ID,
-  measurementId: FIREBASE_MEASUREMENT_ID,
-};
 
 // const firebaseConfig = {
-//   apiKey: "AIzaSyCh-fYz6w3SrZA4XC85rbrFOv4b5TQCLXo",
-//   authDomain: "acn-resale-inventories-dde03.firebaseapp.com",
-//   databaseURL: "https://acn-resale-inventories-dde03-default-rtdb.firebaseio.com",
-//   projectId: "acn-resale-inventories-dde03",
-//   storageBucket: "acn-resale-inventories-dde03.firebasestorage.app",
-//   messagingSenderId: "727649873733",
-//   appId: "1:727649873733:web:31c6731000428aebf3982c",
-//   measurementId: "G-PF5GCHFMTW",
+//   apiKey: FIREBASE_API_KEY,
+//   authDomain: FIREBASE_AUTH_DOMAIN,
+//   databaseURL: FIREBASE_DATABASE_URL,
+//   projectId: FIREBASE_PROJECT_ID,
+//   storageBucket: FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+//   appId: FIREBASE_APP_ID,
+//   measurementId: FIREBASE_MEASUREMENT_ID,
 // };
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCh-fYz6w3SrZA4XC85rbrFOv4b5TQCLXo",
+  authDomain: "acn-resale-inventories-dde03.firebaseapp.com",
+  databaseURL: "https://acn-resale-inventories-dde03-default-rtdb.firebaseio.com",
+  projectId: "acn-resale-inventories-dde03",
+  storageBucket: "acn-resale-inventories-dde03.firebasestorage.app",
+  messagingSenderId: "727649873733",
+  appId: "1:727649873733:web:31c6731000428aebf3982c",
+  measurementId: "G-PF5GCHFMTW",
+};
 
 
 // Initialize Firebase
