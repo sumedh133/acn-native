7948930f2c46563c9f4744e0ae784dfce790b761
SIDDH Jain <siddhjain@SIDDHs-MacBook-Air.local>
2025-04-27 18:24:26 +0530

    file upload component complete

--- app/(tabs)/AddInventoryForm.tsx
+++ app/(tabs)/AddInventoryForm.tsx
@@ -198,6 +198,13 @@ const AddInventoryForm = () => {
             required={component.required}
           />
         );
+        case "Document":
+            return (
+              <Document
+                setDocsToUpload={setDocsToUpload}
+                docsToUpload={docsToUpload}
+              />
+            );
 
       default:
         return null;
@@ -279,7 +286,7 @@ const styles = StyleSheet.create({
   scrollContent: {
     // backgroundColor: "#F5F6F7",
     paddingVertical: 16,
-    paddingHorizontal: 12,
+    paddingHorizontal: 16,
     width: "100%",
   },
   container: {
