66cd93270dda4292a9c6cfc104cb3fdf9ecc873d
Yash Balyan <yashbalyan08@gmail.com>
2025-08-29 18:45:00 +0530

    fetching property in details page

--- app/(pages)/MyBusiness/PropertiesDetailsScreen.tsx
+++ app/(pages)/MyBusiness/PropertiesDetailsScreen.tsx
@@ -1,7 +1,11 @@
+import { selectPropertyStateData } from "@/store/slices/propertySlice";
 import { Text } from "react-native";
 import { View } from "react-native";
+import { useSelector } from "react-redux";
 
 const PropertysDetailsScreen = () => {
+  const property = useSelector(selectPropertyStateData);
+  console.log(property, "chummmaaaaaa de deeeee");
   return (
     <View>
       <Text>jbadjigWAGBUIDAHIS</Text>
