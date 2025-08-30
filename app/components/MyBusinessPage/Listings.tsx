1ae8bf908958fb755051bcfc66a37a68d1684a2e
Yash Balyan <yashbalyan08@gmail.com>
2025-08-30 17:30:00 +0530

    multi select in my business page

--- app/components/MyBusinessPage/Listings.tsx
+++ app/components/MyBusinessPage/Listings.tsx
@@ -1,28 +1,37 @@
 import { View, Text } from "react-native";
 import { MobileHits } from "../property/MobileHits";
+import { Property } from "@/app/types";
 
 interface MyBusinessListingsProps {
   data: any;
   loadMore: any;
   refresh: any;
+  selectedProperties: string[];
+  setSelectedProperties: (selectedProperties: string[]) => void;
+  loading: boolean;
 }
 
 const MyBusinessListings = ({
   data,
   loadMore,
   refresh,
+  selectedProperties,
+  setSelectedProperties,
+  loading,
 }: MyBusinessListingsProps) => {
   return (
     <View className="w-full flex-1">
       <MobileHits
         results={data.allResults}
-        loading={data.loading}
+        loading={data.loading || loading}
         loadingMore={data.loadingMore}
         hasMore={data.hasMore}
         error={data.error}
         totalHits={data.totalHits}
         onLoadMore={loadMore}
         onRefresh={refresh}
+        selectedProperties={selectedProperties}
+        setSelectedProperties={setSelectedProperties}
       />
     </View>
   );
